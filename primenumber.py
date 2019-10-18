8ca5139f5836db636899a0c72b46d967baa9e94e
Swetha Sukumar <swethasukumar14@gmail.com>
2019-10-18 15:51:24 +0530

    Update primenumber.py

--- primenumber.py
+++ primenumber.py
@@ -1,8 +1,9 @@
 start=int(input("Enter the starting value:"))
 end=int(input("Enter the ending value:"))
 for number in range(start,end+1):
-    for x in range(2,number):
-        if (number%x)==0:
-            break
+    if number>1:
+        for x in range(2,number):
+            if (number%x)==0:
+                break
         else:
             print(number)
